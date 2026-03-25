9c96f2880cc3f8e9a498b3ed8d277cc976ff8332
Swapnil <swapnilshingne155@gmail.com>
2026-03-25 10:19:59 +0530

    git ignore added

--- backend/server.js
+++ backend/server.js
@@ -31,13 +31,11 @@ const razorpay = new Razorpay({
 
 // Middleware
 app.use(cors({ 
-  origin: 
-  // [
-  //   'http://localhost:5173', // Local development
-  //   'https://hostel-management-wegk.vercel.app', // Your Vercel frontend
-  //   'https://hostel-management.vercel.app' // Main domain
-  // ]
-  true,
+  origin: [
+    'http://localhost:5173', // Local development
+    'https://hostel-management-wegk.vercel.app', // Your Vercel frontend
+    'https://hostel-management.vercel.app' // Main domain
+  ],
   credentials: true
 }));
 app.use(express.json());
